26f3b5b4326aab91257b153180ea73faa21a881e
hemmu3127 <122113901+hemmu3127@users.noreply.github.com>
2024-05-17 00:11:44 +0530

    Add files via upload

---
+++ Admin/assets/all_product.js
@@ -0,0 +1,167 @@
+import p1_img from "./banganapalli1.jpg";
+import p2_img from "./totapuri1.jpg";
+import p3_img from "./Badami.jpg";
+import p4_img from "./neelam.jpg";
+import p5_img from "./malgova1.jpg";
+import p6_img from "./sinduri.jpg";
+import p7_img from "./alphonso.jpg";
+import p8_img from "./kesar.jpg";
+import p9_img from "./dasheri.jpg";
+import p10_img from "./langra.jpg";
+import p11_img from "./himsagar.jpg";
+import p12_img from "./rumani.jpg";
+import p13_img from "./imampasand.jpg";
+import p14_img from "./banganapalli.png";
+import p15_img from "./malgova.jpg";
+import p16_img from "./kesar.jpg";
+import p17_img from "./alphonso.jpg";
+import p18_img from "./neelam.jpg";
+
+let all_product = [
+  {
+    id: 1,
+    name: "Banganapalli",
+    category: "south",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "Totapuri",
+    category: "south",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 3,
+    name: "Badami",
+    category: "south",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 4,
+    name: "Neelam",
+    category: "south",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 5,
+    name: "Malgova",
+    category: "south",
+    image: p5_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 6,
+    name: "Sinduri",
+    category: "south",
+    image: p6_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 7,
+    name: "Alphonso",
+    category: "north",
+    image: p7_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 8,
+    name: "Kesar",
+    category: "north",
+    image: p8_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 9,
+    name: "Dasheri",
+    category: "north",
+    image: p9_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 10,
+    name: "Langra",
+    category: "north",
+    image: p10_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 11,
+    name: "Himsagar",
+    category: "north",
+    image: p11_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 12,
+    name: "Rumani",
+    category: "north",
+    image: p12_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 13,
+    name: "Himam Pasand",
+    category: "pplr",
+    image: p13_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 14,
+    name: "Banganapalli",
+    category: "pplr",
+    image: p14_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 15,
+    name: "Malgova",
+    category: "pplr",
+    image: p15_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 16,
+    name: "Kesar",
+    category: "pplr",
+    image: p16_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 17,
+    name: "Alphonso",
+    category: "pplr",
+    image: p17_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 18,
+    name: "Neelam",
+    category: "pplr",
+    image: p18_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+];
+
+export default all_product;
---
+++ Admin/assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './banganapalli.png'
+import p2_img from './totapuri.jpg'
+import p3_img from './malgova.jpg'
+import p4_img from './sindhoora.jpg'
+
+let data_product = [
+  {
+    id:1,
+    name:"Banganapalli",
+    image:p1_img,
+    new_price:50.00,
+    old_price:70.00,
+  },
+  {id:2,
+    name:"Totapuri",
+    image:p2_img,
+    new_price:40.00,
+    old_price:60.00,
+  },
+  {id:3,
+    name:"Malgova",
+    image:p3_img,
+    new_price:85.00,
+    old_price:100.50,
+  },
+  {id:4,
+    name:"Sindhoora",
+    image:p4_img,
+    new_price:50.00,
+    old_price:80.00,
+  },
+];
+
+export default data_product;
